2b976ee842a6b3315de98da3f171987144d55e84
VaghelaShyam <166225973+VaghelaShyam@users.noreply.github.com>
2024-04-16 11:24:55 +0530

    45.php

--- 45.php
+++ 45.php
@@ -1,12 +1,37 @@
+<!-- (45)Write script display data using decoded form. -->
+
 <!DOCTYPE html>
 <html>
+<head>
+    <title>Decode Form Data</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
 <body>
-<?php
-$jsonobj = '{"Peter":35,"Ben":37,"Joe":43}';
-$arr = json_decode($jsonobj, true);
-echo $arr["Peter"]."<br>";
-echo $arr["Ben"]."<br>";
-echo $arr["Joe"]."<br>";
-?>
+    <h1>Decode Form Data</h1>
+    <form id="formData">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name"><br>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email"><br>
+        <input type="submit" value="Submit">
+    </form>
+    <div id="displayData"></div>
+
+    <script>
+        $(function(){
+            $('#formData').submit(function(e){
+                e.preventDefault();
+                $.post('', $(this).serialize(), function(response){
+                    $('#displayData').html(response);
+                });
+            });
+        });
+    </script>
+
+    <?php if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['name']) && isset($_POST['email'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        echo "<h2>Form Data:</h2><p>Name: $name</p><p>Email: $email</p>";
+    } ?>
 </body>
 </html>
