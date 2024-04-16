44677062122b2ab14c8217959e1e7edc8e9d0b90
VaghelaShyam <166225973+VaghelaShyam@users.noreply.github.com>
2024-04-16 11:22:30 +0530

    43.php

--- 43.php
+++ 43.php
@@ -1,21 +1,44 @@
+<!-- (43)Write script form data display using serialize. -->
+
 <!DOCTYPE html>
 <html>
 <head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-<script>
-$(document).ready(function(){
- $("button").click(function(){
- $("div").text($("form").serialize());
- });
-});
-</script>
+    <title>Serialize Form Data</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
-<form action="">
- First name: <input type="text" name="FirstName" value="Mickey"><br>
- Last name: <input type="text" name="LastName" value="Mouse"><br>
-</form>
-<button>Serialize form values</button>
-<div></div>
+    <h1>Serialize Form Data</h1>
+    <form id="formData">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name"><br>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email"><br>
+        <input type="submit" value="Submit">
+    </form>
+    <div id="displayData"></div>
+
+    <script>
+        $(document).ready(function(){
+            $('#formData').submit(function(e){
+                e.preventDefault();
+
+                var formData = $(this).serialize();
+                $.post('', formData, function(response){
+                    $('#displayData').html(response);
+                });
+            });
+        });
+    </script>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['name']) && isset($_POST['email'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+
+        echo "<h2>Form Data:</h2>";
+        echo "<p>Name: $name</p>";
+        echo "<p>Email: $email</p>";
+    }
+    ?>
 </body>
 </html>
